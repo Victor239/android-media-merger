72b82be0113cb35de04663c116ff95f7b619da8c
Alexey Kuznetsov <axet@me.com>
2017-03-31 14:10:31 +0300

    allow select sdcard

--- app/src/main/java/com/github/axet/mover/activities/MainActivity.java
+++ app/src/main/java/com/github/axet/mover/activities/MainActivity.java
@@ -162,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                 path.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        final OpenFileDialog f = new OpenFileDialog(MainActivity.this, OpenFileDialog.DIALOG_TYPE.FOLDER_FIALOG);
+                        final OpenFileDialog f = new OpenFileDialog(MainActivity.this, OpenFileDialog.DIALOG_TYPE.FOLDER_DIALOG);
                         f.setCurrentPath(new File(p));
                         f.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                             @Override
@@ -327,7 +327,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         browse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final OpenFileDialog f = new OpenFileDialog(MainActivity.this, OpenFileDialog.DIALOG_TYPE.FOLDER_FIALOG);
+                final OpenFileDialog f = new OpenFileDialog(MainActivity.this, OpenFileDialog.DIALOG_TYPE.FOLDER_DIALOG);
 
                 final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
                 String path = sharedPref.getString(MoverApplication.STORAGE, null);
@@ -364,7 +364,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final OpenFileDialog f = new OpenFileDialog(MainActivity.this, OpenFileDialog.DIALOG_TYPE.FOLDER_FIALOG);
+                final OpenFileDialog f = new OpenFileDialog(MainActivity.this, OpenFileDialog.DIALOG_TYPE.FOLDER_DIALOG);
                 f.setCurrentPath(Environment.getExternalStorageDirectory());
                 f.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
