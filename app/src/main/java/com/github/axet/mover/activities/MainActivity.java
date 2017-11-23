19a6f948bfd9adae17e9a8f8cb6937ad3fb729ca
Alexey Kuznetsov <axet@me.com>
2017-11-23 21:42:08 +0300

    set def

--- app/src/main/java/com/github/axet/mover/activities/MainActivity.java
+++ app/src/main/java/com/github/axet/mover/activities/MainActivity.java
@@ -348,7 +348,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
             public void onClick(View v) {
                 String p = storage.getStoragePath();
                 Uri old = storage.getStoragePath(p);
-                OpenStorageChoicer choicer = new OpenStorageChoicer(OpenFileDialog.DIALOG_TYPE.FOLDER_DIALOG, false) {
+                choicer = new OpenStorageChoicer(storage, OpenFileDialog.DIALOG_TYPE.FOLDER_DIALOG, false, getString(R.string.folder_name)) {
                     @Override
                     public void onResult(Uri uri) {
                         final SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
@@ -370,8 +370,6 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                 };
                 MainActivity.this.choicer = choicer;
                 choicer.setTitle(getString(R.string.pref_storage_title));
-                choicer.def = getString(R.string.folder_name);
-                choicer.setStorage(storage);
                 choicer.setPermissionsDialog(MainActivity.this, FileObserverService.PERMISSIONS, RESULT_PERMS_STORAGE);
                 choicer.setStorageAccessFramework(MainActivity.this, RESULT_STORAGE);
                 choicer.show(old);
@@ -469,7 +467,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
             String p = storage.getStoragePath();
             Uri old = storage.getStoragePath(p);
-            OpenStorageChoicer choicer = new OpenStorageChoicer(OpenFileDialog.DIALOG_TYPE.FOLDER_DIALOG, false) {
+            choicer = new OpenStorageChoicer(storage, OpenFileDialog.DIALOG_TYPE.FOLDER_DIALOG, false, getString(R.string.folder_name)) {
                 @Override
                 public void onResult(Uri uri) {
                     final SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
@@ -496,10 +494,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                     SettingsActivity.warninig(MainActivity.this); // mandatory permissions, show warning
                 }
             };
-            MainActivity.this.choicer = choicer;
             choicer.setTitle(getString(R.string.pref_storage_title));
-            choicer.def = getString(R.string.folder_name);
-            choicer.setStorage(storage);
             choicer.setPermissionsDialog(MainActivity.this, FileObserverService.PERMISSIONS, RESULT_PERMS_ENABLE);
             choicer.setStorageAccessFramework(MainActivity.this, RESULT_ENABLE);
             if (FileObserverService.isEnabled(this, true) || !b) {
