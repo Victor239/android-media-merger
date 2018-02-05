a3344093fb6a3050dd70bf13be777c5922728e69
Alexey Kuznetsov <axet@me.com>
2018-02-05 16:24:46 +0300

    cleanups

--- app/src/main/java/com/github/axet/mover/activities/SettingsActivity.java
+++ app/src/main/java/com/github/axet/mover/activities/SettingsActivity.java
@@ -127,8 +127,7 @@ public class SettingsActivity extends AppCompatSettingsThemeActivity implements 
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
             if (item.getItemId() == android.R.id.home) {
-                getActivity().finish();
-                MainActivity.startActivity(getContext());
+                getActivity().onBackPressed();
                 return true;
             }
             return super.onOptionsItemSelected(item);
@@ -221,5 +220,11 @@ public class SettingsActivity extends AppCompatSettingsThemeActivity implements 
 
     public void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        MainActivity.startActivity(this);
     }
 }
